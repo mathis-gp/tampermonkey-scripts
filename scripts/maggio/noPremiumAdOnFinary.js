a5ff36e3d45fa6b5d5236c6618bf21968ccdc328
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2026-06-16 11:22:29 +0200

    fix(noPremiumAdOnFinary): check url on url change

--- scripts/maggio/noPremiumAdOnFinary.js
+++ scripts/maggio/noPremiumAdOnFinary.js
@@ -1,19 +1,32 @@
 // ==UserScript==
 // @name         No premium ad on Finary
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.0.1
+// @version      0.0.2
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noPremiumAdOnFinary.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noPremiumAdOnFinary.js
 // @description  No premium ad on Finary
 // @author       Maggio
-// @match        https://app.finary.com/v2/premium?origin=finary_plus_modal
+// @match        https://app.finary.com/v2/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=finary.com
-// @grant        none
+// @grant        window.onurlchange
 // @run-at       document-start
 // ==/UserScript==
 
 (function () {
   'use strict'
 
-  window.location.href = 'https://app.finary.com/v2/'
+  const PREMIUM_MODAL_URL = 'https://app.finary.com/v2/premium?origin=finary_plus_modal'
+  const HOME_URL = 'https://app.finary.com/v2/'
+
+  function redirectIfPremiumModal () {
+    if (window.location.href.includes(PREMIUM_MODAL_URL)) {
+      window.location.replace(HOME_URL)
+    }
+  }
+
+  redirectIfPremiumModal()
+
+  if (window.onurlchange === null) {
+    window.addEventListener('urlchange', redirectIfPremiumModal)
+  }
 })()
